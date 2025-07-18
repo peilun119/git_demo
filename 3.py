95bac83f53f7c223b42e960ebcc34bce681d87c5
sghost <aa8954158@gmail.com>
2025-07-18 20:06:32 +0800

    '修改拿掉else'

--- 3.py
+++ 3.py
@@ -7,15 +7,15 @@ while True:
         a = input('請輸入數字a:')
         if (a == 'exit'):
             break
-        else:
-            a = eval(a)
-            b = eval(input('請輸入數字b:'))
 
-            if (a > b):
-                print(f'a比b大{a-b}')
-            elif (b > a):
-                print(f'b比a大{b-a}')
-            else:
-                print('a跟b一樣大')
+        a = eval(a)
+        b = eval(input('請輸入數字b:'))
+
+        if (a > b):
+            print(f'a比b大{a-b}')
+        elif (b > a):
+            print(f'b比a大{b-a}')
+        else:
+            print('a跟b一樣大')
     except Exception as e:
         print(e)
