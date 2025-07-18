078d83c5db23cd67dc09b98e5dc33b4782961615
sghost <aa8954158@gmail.com>
2025-07-18 19:57:20 +0800

    '修改程式'

--- 3.py
+++ 3.py
@@ -1,12 +1,21 @@
-try:
-    a = eval(input('請輸入數字a:'))
-    b = eval(input('請輸入數字b:'))
+# 1. 加入例外處理
+# 2. 重複輸入,直到a -> exit 離開,使用while True
 
-    if (a > b):
-        print(f'a比b大{a-b}')
-    elif (b > a):
-        print(f'b比a大{b-a}')
-    else:
-        print('a跟b一樣大')
-except Exception as e:
-    print(e)
+
+while True:
+    try:
+        a = input('請輸入數字a:')
+        if (a == 'exit'):
+            break
+        else:
+            a = eval(a)
+            b = eval(input('請輸入數字b:'))
+
+            if (a > b):
+                print(f'a比b大{a-b}')
+            elif (b > a):
+                print(f'b比a大{b-a}')
+            else:
+                print('a跟b一樣大')
+    except Exception as e:
+        print(e)
